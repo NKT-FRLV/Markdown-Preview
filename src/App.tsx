bc7920baa68d45c9b420cb4f7e3dbbdf7cc959bb
GertsDev <hello@gerts.dev>
2025-07-11 13:15:14 -0400

    markdown init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [markdownInput, setMarkdownInput] = useState('');
 
   return (
-    <>
-      <div className="bg-red-200">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App h-screen w-full p-4 bg-slate-300">
+      <div className="main-Wrapper">
+        <Editor />
+        <Preview />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
