5caba98ca64da161123a6a7639d5802a9ffd7cfe
FROL <nkt.frlv7@yandex.ru>
2025-07-11 19:56:22 +0200

    feture: MD Preview component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import './App.css';
+import Markdown from 'react-markdown';
 import Editor from './components/Editor';
 import Preview from './components/Preview';
+
+// import {mockMarkdown} from './mocks/mock'
 
 function App() {
   const [markdownInput, setMarkdownInput] = useState('');
@@ -10,7 +13,7 @@ function App() {
     <div className="App h-screen w-full p-4 bg-slate-300">
       <div className="main-Wrapper">
         <Editor />
-        <Preview />
+        <Preview htmlOutput={<Markdown>{markdownInput}</Markdown>}/>
       </div>
     </div>
   );
